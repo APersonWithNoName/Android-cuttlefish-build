7a3f240ea3f40e5e1a3db6dcdb775fc1e254429f
APersonWithNoName <wyb20102024@outlook.com>
2025-12-31 20:43:54 +0800

    Fix Dockerfile

--- shells/cleanenv.sh
+++ shells/cleanenv.sh
@@ -4,7 +4,7 @@ echo "Cleaning host environment."
 
 function remove_pkg(){
     echo "--- Remove: $@"
-    sudo apt remove $@ -y --purge 
+    sudo apt remove $@ -y --purge > /dev/null 
 }
 
 remove_pkg clang-*-16 clang-*-17 clang-*-18
@@ -14,5 +14,5 @@ remove_pkg g++-12 g++-13 g++-14
 remove_pkg mysql-server-8.0 mysql-client-8.0
 remove_pkg php8.3 php8.3-*
 
-sudo apt autoremove -y --purge
+sudo apt autoremove -y --purge /dev/null
 
